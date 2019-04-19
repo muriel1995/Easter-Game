f59bca6a94346dca01931a6a07264a901cf40be2
Urth <dragondor@protonmail.com>
2019-04-19 11:28:24 +0200

    component Pill added

--- src/components/GameBoard/Map/Map.jsx
+++ src/components/GameBoard/Map/Map.jsx
@@ -27,36 +27,36 @@ export default class Map extends Component {
                             <Col xs="2" className="col2"></Col>
                         </Row>
                         <Row>
-                            <Col xs="2" className="path">2</Col>
+                            <Col xs="2" id="2" className="path">2</Col>
                             <Col xs="2" className="col2"></Col>
-                            <Col xs="2" className="path">10</Col>
+                            <Col xs="2" id ="10" className="path">10</Col>
                             <Col xs="2" className="col2"></Col>
-                            <Col xs="2" className="path">17</Col>
-                            <Col xs="2" className="col2"></Col>
-                        </Row>
-                        <Row>
-                            <Col xs="2" className="path">3</Col>
-                            <Col xs="2" className="col"></Col>
-                            <Col xs="2" className="path">9</Col>
-                            <Col xs="2" className="col"></Col>
-                            <Col xs="2" className="path">18</Col>
-                            <Col xs="2" className="col"></Col>
-                        </Row>
-                        <Row>
-                            <Col xs="2" className="path">4</Col>
-                            <Col xs="2" className="col2"></Col>
-                            <Col xs="2" className="path">8</Col>
-                            <Col xs="2" className="col2"></Col>
-                            <Col xs="2" className="path">19</Col>
+                            <Col xs="2" id ="17" className="path">17</Col>
                             <Col xs="2" className="col2"></Col>
                         </Row>
                         <Row>
-                            <Col xs="2" className="path">5</Col>
-                            <Col xs="2" className="path">6</Col>
-                            <Col xs="2" className="path">7</Col>
+                            <Col xs="2" id ="3"className="path">3</Col>
+                            <Col xs="2" className="col"></Col>
+                            <Col xs="2" id ="9" className="path">9</Col>
+                            <Col xs="2" className="col"></Col>
+                            <Col xs="2" id ="18" className="path">18</Col>
+                            <Col xs="2" className="col"></Col>
+                        </Row>
+                        <Row>
+                            <Col xs="2" id ="4" className="path">4</Col>
                             <Col xs="2" className="col2"></Col>
-                            <Col xs="2" className="path">20</Col>
-                            <Col xs="2" className="Line">Finish</Col>
+                            <Col xs="2" id ="8" className="path">8</Col>
+                            <Col xs="2" className="col2"></Col>
+                            <Col xs="2" id ="19" className="path">19</Col>
+                            <Col xs="2" className="col2"></Col>
+                        </Row>
+                        <Row>
+                            <Col xs="2" id ="5" className="path">5</Col>
+                            <Col xs="2" id ="6" className="path">6</Col>
+                            <Col xs="2" id ="7" className="path">7</Col>
+                            <Col xs="2" className="col2"></Col>
+                            <Col xs="2" id ="20"className="path">20</Col>
+                            <Col xs="2" id ="finish" className="Line">Finish</Col>
 
                         </Row>
                     </div>
